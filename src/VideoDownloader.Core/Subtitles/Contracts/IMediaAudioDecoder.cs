7b197c604780b9dc64602466dd4efc8e2b4f2727
Liu ZhiDong <hl13446882@gmail.com>
2026-09-19 14:46:47 +0800

    refactor(subtitles): make audio decoding local-file only

--- src/VideoDownloader.Core/Subtitles/Contracts/IMediaAudioDecoder.cs
+++ src/VideoDownloader.Core/Subtitles/Contracts/IMediaAudioDecoder.cs
@@ -1,15 +1,7 @@
-using VideoDownloader.Core.Models;
-
 namespace VideoDownloader.Core.Subtitles.Contracts;
 
 public interface IMediaAudioDecoder
 {
-    Task<AudioChunk> DecodeAsync(
-        MediaVariant variant,
-        TimeSpan start,
-        TimeSpan duration,
-        CancellationToken cancellationToken = default);
-
     Task<AudioChunk> DecodeLocalFileAsync(
         string filePath,
         TimeSpan start,
