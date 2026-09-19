09159ac850dd372053bd6d2577d73fee65d5cf08
Liu ZhiDong <hl13446882@gmail.com>
2026-09-19 09:06:53 +0800

    feat(subtitles): wire subtitle services into app composition

--- src/VideoDownloader.UI/AppServiceComposition.cs
+++ src/VideoDownloader.UI/AppServiceComposition.cs
@@ -2,6 +2,7 @@ using System.IO;
 using Microsoft.Extensions.DependencyInjection;
 using VideoDownloader.Infrastructure;
 using VideoDownloader.Infrastructure.Configuration;
+using VideoDownloader.Infrastructure.Subtitles;
 using VideoDownloader.UI.Localization;
 using VideoDownloader.UI.ViewModels;
 
@@ -28,6 +29,7 @@ public static class AppServiceComposition
         var services = new ServiceCollection();
         services.AddSingleton(store);
         services.AddVideoDownloaderInfrastructure(options);
+        services.AddVideoDownloaderSubtitles();
         services.AddSingleton<LocalizationService>();
         if (singletonUi)
         {
